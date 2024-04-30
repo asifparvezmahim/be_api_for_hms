c9a5c9f4a6fd05190a97f6318410d0c0a2b90867
Md.Asif Parvez Mahim <100875772+asifparvezmahim@users.noreply.github.com>
2024-04-30 17:17:31 +0600

    dept update

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from . import models
 
 class doctorSerializers(serializers.ModelSerializer):
+    department=serializers.StringRelatedField(many=False)
     class Meta:
         model=models.Doctor
         fields="__all__"
--- doctor/views.py
+++ doctor/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from . import models
 from . import serializers
-
 # Create your views here.
 class doctorViewset(viewsets.ModelViewSet):
     queryset = models.Doctor.objects.all()
     serializer_class = serializers.doctorSerializers
+
